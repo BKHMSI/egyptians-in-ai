7fafee6e15f95549817a19c6acb8b2cbcca435db
Badr AlKhamissi <bkhmsi@Badrs-MacBook-Pro.local>
2023-03-18 14:02:30 +0100

    updated website

--- src/populate.py
+++ src/populate.py
@@ -23,25 +23,37 @@ if __name__ == "__main__":
 
     for idx in range(len(responses)):
         response = responses.iloc[idx]
-        name = response["Name"].strip()
-        photo_link = response["Personal Photo Link"]
-        scholar = response["Google Scholar Profile"].strip()
-        linkedin = response["LinkedIn Profile"]
-        twitter = response["Twitter Profile"]
-        website = response["Personal Website"]
-        interests = [interest.strip() for interest in response["Research Interests"].split(",")] if not pd.isna(response["Research Interests"]) else []
-        if name not in names:
-            researchers += [{
-                "name": name,
-                "affliation": response["Affiliation"].strip(),
-                "position": response["Position"].strip(),
-                "hindex": -1,
-                "photo": photo_link if not is_nan(photo_link) else "./assets/images/default.jpg",
-                "scholar": scholar,
-                "linkedin": "" if is_nan(linkedin) else linkedin,
-                "website": "" if is_nan(website) else website,
-                "twitter": "" if is_nan(twitter) else twitter,
-                "interests": interests
-            }]
-        
+        add_flag = response["Add or Update"] == "Add"
+
+        if add_flag:
+            name = response["Name"].strip()
+            photo_link = response["Personal Photo Link"]
+            scholar = str(response["Google Scholar Profile"]).strip()
+            linkedin = response["LinkedIn Profile"]
+            twitter = response["Twitter Profile"]
+            website = response["Personal Website"]
+            interests = [interest.strip() for interest in response["Research Interests"].split(",")] if not pd.isna(response["Research Interests"]) else []
+            if name not in names:
+                print(f"> [Add] {name}")
+                researchers += [{
+                    "name": name,
+                    "affliation": response["Affiliation"].strip(),
+                    "position": response["Position"].strip(),
+                    "hindex": -1,
+                    "photo": photo_link if not is_nan(photo_link) else "./assets/images/default.jpg",
+                    "scholar": scholar,
+                    "linkedin": "" if is_nan(linkedin) else linkedin,
+                    "website": "" if is_nan(website) else website,
+                    "twitter": "" if is_nan(twitter) else twitter,
+                    "interests": interests
+                }]
+        else:
+            name = response["Name.1"].strip()
+            photo_link = response["Personal Photo Link.1"]
+            scholar = str(response["Google Scholar Profile.1"]).strip()
+            linkedin = response["LinkedIn Profile.1"]
+            twitter = response["Twitter Profile.1"]
+            website = response["Personal Website.1"]
+            interests = [interest.strip() for interest in response["Research Interests.1"].split(",")] if not pd.isna(response["Research Interests.1"]) else []
+            print(f"> [Update] {name}")
     write_json(out_file, researchers)
